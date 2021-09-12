a8ccf9e93baf0b25386271311d5c23ac9a72cbe4
Tze Ming <e0311042@u.nus.edu>
2021-09-12 23:34:12 +0800

    Init theme

---
+++ tamp/config/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6F4E37",
+    },
+    secondary: {
+      main: "#37586F",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#F1EDEB",
+    },
+  },
+});
+
+export default theme;
