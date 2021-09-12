d68921b15d85322fc25fa24ea53ba611e3ddf610
Tze Ming <e0311042@u.nus.edu>
2021-09-12 16:23:45 +0800

    Init create shortened url

--- slimshady/pages/api/urls.js
+++ slimshady/pages/api/urls.js
@@ -1,4 +1,5 @@
 import clientPromise from "../../lib/mongodb";
+import { nanoid } from "nanoid";
 
 let db;
 
@@ -15,14 +16,40 @@ export default async (req, res) => {
       return;
     }
 
+    // initiate db connection
     if (!db) {
       const client = await clientPromise;
       db = client.db();
     }
 
-    const urls = await db.collection("urls").find({}).toArray();
+    // check if long url is already in db
+    const urls = await db
+      .collection("urls")
+      .find({
+        longURL: longURL,
+      })
+      .toArray();
+    if (urls.length > 0) {
+      res.status(200).json(urls[0]);
+      return;
+    }
 
-    res.status(201).json({ message: "ok", longURL: longURL });
+    // generate unique 8-letter short url
+    let shortURL = nanoid(8);
+    // max 10 tries
+    for (let i = 0; i < 10; i++) {
+      if (db.collection("urls").find({ shortURL: shortURL }).toArray().length <= 0) {
+        break;
+      }
+      shortURL = nanoid(8); // regen a new id
+    }
+
+    const _id = await db.collection("urls").insertOne({
+      shortURL: `/${shortURL}`,
+      longURL: longURL,
+    });
+
+    res.status(200).json(_id.ops[0]);
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
