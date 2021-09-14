ea5ba271288b24e9635352ca377fe7f3a63af8f7
Tze Ming <e0311042@u.nus.edu>
2021-09-14 19:57:09 +0800

    Add copied snackbar

--- tamp/features/ShortURLPage/index.js
+++ tamp/features/ShortURLPage/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button, Card, makeStyles, TextField, Typography } from "@material-ui/core";
-import { FileCopyOutlined } from "@material-ui/icons";
+import React, { Fragment, useState } from "react";
+import { Button, Card, makeStyles, Snackbar, TextField, Typography, IconButton } from "@material-ui/core";
+import { Close, FileCopyOutlined } from "@material-ui/icons";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import { AnimateWrapper } from "../Animate";
@@ -47,35 +47,53 @@ const useStyles = makeStyles((theme) => ({
 
 const ShortURLPage = ({ paginate, res }) => {
   const classes = useStyles();
-  console.log(res);
-  console.log(paginate);
+  const [sbOpen, setSbOpen] = useState(false);
 
   return (
-    <AnimateWrapper className={classes.root}>
-      <img src="/latte.svg" alt="tamper" className={classes.image} />
-      <Typography variant="h1" className={classes.headerText}>
-        Here ya go
-      </Typography>
-      <Card variant="outlined" className={classes.cardRoot}>
-        <TextField
-          className={classes.textField}
-          margin="dense"
-          fullWidth
-          variant="outlined"
-          placeholder="Your URL to tamp"
-          value={`${window ? window.location.href : ""}${res.shortURL}`}
-          disabled
-        />
-        <CopyToClipboard text={`${window ? window.location.href : ""}${res.shortURL}`}>
-          <Button color="primary" fullWidth variant="contained" startIcon={<FileCopyOutlined />}>
-            Copy
+    <Fragment>
+      <AnimateWrapper className={classes.root}>
+        <img src="/latte.svg" alt="tamper" className={classes.image} />
+        <Typography variant="h1" className={classes.headerText}>
+          Here ya go
+        </Typography>
+        <Card variant="outlined" className={classes.cardRoot}>
+          <TextField
+            className={classes.textField}
+            margin="dense"
+            fullWidth
+            variant="outlined"
+            placeholder="Your URL to tamp"
+            value={`${window ? window.location.href : ""}${res.shortURL}`}
+            disabled
+          />
+          <CopyToClipboard text={`${window ? window.location.href : ""}${res.shortURL}`}>
+            <Button
+              color="primary"
+              fullWidth
+              variant="contained"
+              startIcon={<FileCopyOutlined />}
+              onClick={() => setSbOpen(true)}
+            >
+              Copy
+            </Button>
+          </CopyToClipboard>
+          <Button color="primary" fullWidth onClick={() => paginate(1)}>
+            Again!
           </Button>
-        </CopyToClipboard>
-        <Button color="primary" fullWidth onClick={() => paginate(1)}>
-          Again!
-        </Button>
-      </Card>
-    </AnimateWrapper>
+        </Card>
+      </AnimateWrapper>
+      <Snackbar
+        open={sbOpen}
+        onClose={() => setSbOpen(false)}
+        autoHideDuration={3000}
+        message="Copied!"
+        action={
+          <IconButton size="small" aria-label="close" color="inherit" onClick={() => setSbOpen(false)}>
+            <Close fontSize="small" />
+          </IconButton>
+        }
+      />
+    </Fragment>
   );
 };
 
